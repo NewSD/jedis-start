4ecc401036c5e6e30aedc860e5474cac7299bc16
niuhaideng <niuhaideng@163.com>
2019-03-11 12:03:59 +0800

    first commit

---
+++ src/main/java/com/niu/jedis/JedisStartApplication.java
@@ -0,0 +1,13 @@
+package com.niu.jedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JedisStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JedisStartApplication.class, args);
+	}
+
+}
